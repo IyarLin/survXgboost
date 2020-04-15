7d4ccc7ae14eba5cb4f484c11a79d42532241e16
Alexey Nikolaev <alexey_nikolaev@ymail.com>
2020-04-15 14:44:39 +0300

    added conditional_after

--- R/predict.xgb.Booster.surv.R
+++ R/predict.xgb.Booster.surv.R
@@ -13,7 +13,7 @@
 #' @seealso \code{\link{xgb.train.surv}}
 #' @export
 
-predict.xgb.Booster.surv <- function(object, newdata, type = "risk", times = NULL) {
+predict.xgb.Booster.surv <- function(object, newdata, type = "risk", times = NULL, conditional_after = NULL) {
   class(object) <- "xgb.Booster"
   risk <- xgboost:::predict.xgb.Booster(object, newdata)
   if (type == "risk") {
@@ -26,7 +26,39 @@ predict.xgb.Booster.surv <- function(object, newdata, type = "risk", times = NUL
     } else {
       times <- object$baseline_hazard[, 2]
     }
-    surv <- exp(risk %*% -matrix(object$baseline_hazard[, 1], nrow = 1))
+    
+    if (!is.null(conditional_after)) {
+      baseline_hazard <- object$baseline_hazard
+
+      interpolate_at_times <- function(baseline_hazard, times) {
+        haz_max <- max(baseline_hazard$hazard)
+        haz_min <- min(baseline_hazard$hazard)
+        interp <- function(time) {
+          return(approx(
+            x = baseline_hazard$time, y = baseline_hazard$hazard, xout = time, yleft = haz_min, yright = haz_max
+            )$y
+          )
+        }
+        return(lapply(times, interp))
+      }
+      
+      n <- nrow(newdata)
+      
+      #times_to_evaluate_at <- t(matrix(rep(times, n), ncol = n)) + conditional_after
+      surv <- list()
+      for (i in 1:n) {
+        times_to_evaluate_at <- times + conditional_after[i]
+        c_0 <- interpolate_at_times(baseline_hazard, times_to_evaluate_at)
+        c_0_conditional_after <- unlist(c_0[1], use.names=FALSE)
+        c_0 <- pmax(0, unlist(c_0, use.names=FALSE) - c_0_conditional_after)
+        surv <- append(surv, exp(risk[i] * -1.0 * c_0))
+      }
+      surv <- matrix(surv, nrow = n)
+    } else {
+      surv <- exp(risk %*% -matrix(object$baseline_hazard[, 1], nrow = 1))
+    }
+    
+    #surv <- exp(risk %*% -matrix(object$baseline_hazard[, 1], nrow = 1))
     surv <- surv[, findInterval(times, object$baseline_hazard[, 2])]
     colnames(surv) <- times
     return(surv)
