c5819510c3cbb3fe6a350319cc248be1fe90a4cb
Alexey Nikolaev <alexey_nikolaev@ymail.com>
2021-01-15 14:32:59 +0200

    Surv to age

--- R/predict.xgb.Booster.surv.R
+++ R/predict.xgb.Booster.surv.R
@@ -13,56 +13,18 @@
 #' @seealso \code{\link{xgb.train.surv}}
 #' @export
 
-predict.xgb.Booster.surv <- function(object, newdata, type = "risk", times = NULL, conditional_after = NULL) {
+predict.xgb.Booster.surv <- function(object, newdata, type = "risk", plan_length, times = NULL) {
   class(object) <- "xgb.Booster"
   risk <- xgboost:::predict.xgb.Booster(object, newdata)
   if (type == "risk") {
     return(risk)
-  } else if (type == "surv") {
-    if (!is.null(times)) {
-      if (max(times) > max(object$baseline_hazard[, 2])) {
-        object$baseline_hazard <- rbind(object$baseline_hazard, c(max(object$baseline_hazard[, 1]), max(times)))
-      }
-    } else {
-      times <- object$baseline_hazard[, 2]
-    }
-    
-    if (!is.null(conditional_after)) {
-      baseline_hazard <- object$baseline_hazard
-
-      interpolate_at_times <- function(baseline_hazard, times) {
-        haz_max <- max(baseline_hazard$hazard)
-        haz_min <- min(baseline_hazard$hazard)
-        interp <- function(time) {
-          return(approx(
-            x = baseline_hazard$time, y = baseline_hazard$hazard, xout = time, yleft = haz_min, yright = haz_max
-            )$y
-          )
-        }
-        return(lapply(times, interp))
-      }
-      
-      n <- nrow(newdata)
-      
-      #times_to_evaluate_at <- t(matrix(rep(times, n), ncol = n)) + conditional_after
-      surv <- list()
-      for (i in 1:n) {
-        times_to_evaluate_at <- times + conditional_after[i]
-        c_0 <- interpolate_at_times(baseline_hazard, times_to_evaluate_at)
-        c_0_conditional_after <- unlist(c_0[1], use.names=FALSE)
-        c_0 <- pmax(0, unlist(c_0, use.names=FALSE) - c_0_conditional_after)
-        surv <- append(surv, exp(risk[i] * -1.0 * c_0))
-      }
-      surv <- matrix(surv, nrow = n, byrow = TRUE)
-    } else {
-      surv <- exp(risk %*% -matrix(object$baseline_hazard[, 1], nrow = 1))
-    }
-    
-    #surv <- exp(risk %*% -matrix(object$baseline_hazard[, 1], nrow = 1))
-    surv <- surv[, findInterval(times, object$baseline_hazard[, 2])]
-    colnames(surv) <- times
-    return(surv)
+  } else if (type == "age") {
+    a <- object$a
+    b <- object$b
+    pred <- exp(a + b * log(risk))
+    pred_rounded <- round(pred / plan_length) * plan_length
+    return(pred_rounded)
   } else {
-    stop('type must be one of "risk", "surv"')
+    stop('type must be one of "risk", "age"')
   }
 }
